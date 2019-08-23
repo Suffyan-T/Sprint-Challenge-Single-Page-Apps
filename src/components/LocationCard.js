107d5b0b1efea23d786e76eff15e08b16e770f7b
Suffyan Tariq <suffyan.tariq@gmail.com>
2019-08-23 09:44:48 -0700

    Card I Loading Data Successfully

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { Card, Icon, Image } from "semantic-ui-react";
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+export default function LocationCard({ loc }) {
+  return (
+    <Card>
+      <Card.Content>
+        <Card.Header>{loc.name}</Card.Header>
+        <Card.Meta>{loc.type}</Card.Meta>
+        <Card.Description> {loc.residents.length} Residents</Card.Description>
+      </Card.Content>
+    </Card>
+  );
 }
+
